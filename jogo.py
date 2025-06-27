8884a8bd26adaeec3e0961129fd9bf23cbdae8d9
Ry-devr <rtmz.developer@gmail.com>
2025-06-27 11:38:37 -0300

    mudanças na divisão

--- jogo.py
+++ jogo.py
@@ -1,10 +1,10 @@
-import random
+from random import randrange
 
-num1 = random.randrange(0, 100)
-num2 = random.randrange(0, 100)
+num1 = randrange(0, 100)
+num2 = randrange(0, 100)
 
-def adiçao():
-    resposta = int(input('{} + {}\n=> '.format(num1, num2)))
+def adiçao(): #função de adiçao
+    resposta = int(input(f'{num1} + {num2}\n=> '))
 
     while True:
         if num1 + num2 == resposta:
@@ -13,10 +13,10 @@ def adiçao():
 
         else:
             print('tente novamente')
-            resposta = int(input('{} - {}\n=> '.format(num1, num2)))
+            resposta = int(input(f'{num1} - {num2}\n=> '))
 
-def subtração():
-    resposta = int(input('{} - {}\n=> '.format(num1, num2)))
+def subtração(): #função de subtração
+    resposta = int(input(f'{num1} - {num2}\n=> '))
 
     while True:
         if num1 - num2 == resposta:
@@ -25,10 +25,10 @@ def subtração():
 
         else:
             print('tente novamente')
-            resposta = int(input('{} - {}=\n=> '.format(num1, num2)))
+            resposta = int(input(f'{num1} - {num2}=\n=> '))
 
-def mutiplicão():
-    resposta = int(input('{} x {}\n=> '.format(num1, num2)))
+def mutiplicão(): #funçao de mutiplicação
+    resposta = int(input(f'{num1} x {num2}\n=> '))
 
     while True:
         if num1 * num2 == resposta:
@@ -37,19 +37,21 @@ def mutiplicão():
 
         else:
             print('tente novamente')
-            resposta = int(input('{} x {}=\n=> '.format(num1, num2)))
+            resposta = int(input(f'{num1} x {num2}=\n=> '))
 
-def divisão():
-    resposta = int(input('{} / {}=\n=> '.format(num1, num2)))
-
+def divisão(): #funçao de divisão
+    resposta = float(input(f'{num1} / {num2}=\n=> '))
+    div = num1 / num2
+    formatado = int(div * 10) /10 #faz o resutado da divisão ficar com uma casa antes da virgula ex: 10.233434 = 10.2
+    
     while True:
-        if num1 / num2 == resposta:
+        if abs(resposta - formatado) < 0.01: #faz ter uma toleracia de erro de 0.01 assim
             print('parabens vc acertou')
             break
 
         else:
             print('tente novamente')
-            resposta = int(input('{} / {}=\n=> '.format(num1, num2)))
+            resposta = float(input(f'{num1} / {num2}=\n=> '))
 
 print('-='*20)
 print('bem vindo ao jodo')
